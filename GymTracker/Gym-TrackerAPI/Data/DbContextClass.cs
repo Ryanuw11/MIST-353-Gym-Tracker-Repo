2752e1e17234c56b508c5e8d7dedaabb87df4a66
dawalter123 <143112522+dawalter123@users.noreply.github.com>
2024-12-06 21:07:55 -0500

    Update DbContextClass.cs

--- GymTracker/Gym-TrackerAPI/Data/DbContextClass.cs
+++ GymTracker/Gym-TrackerAPI/Data/DbContextClass.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 
 
-
 namespace GymTrackersAPI.Data
 {
 
@@ -19,6 +18,8 @@ namespace GymTrackersAPI.Data
         public DbSet<GymLoc> GymLoc { get; set; }
         public DbSet<Exercise> Exercise { get; set; }
         public DbSet<Apperal> Apperal { get; set; }
+        public DbSet<CustomerEmail> CustomerEmails { get; set; }
+        public DbSet<WeatherData> WeatherData { get; set; }
 
     }
 }
