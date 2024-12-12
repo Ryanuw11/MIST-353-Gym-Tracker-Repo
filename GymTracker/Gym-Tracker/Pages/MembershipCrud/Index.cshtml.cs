020dbf403b5e904f016a47839d15ba9df4d7d9c4
Ryanuw11 <156019247+Ryanuw11@users.noreply.github.com>
2024-12-11 22:20:40 -0500

    test

---
+++ GymTracker/Gym-Tracker/Pages/MembershipCrud/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Gym_Tracker.Data;
+
+namespace Gym_Tracker.Pages.MembershipCrud
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Gym_Tracker.Data.GymTrackerDbContext _context;
+
+        public IndexModel(Gym_Tracker.Data.GymTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Membership> Membership { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Membership = await _context.Memberships.ToListAsync();
+        }
+    }
+}
